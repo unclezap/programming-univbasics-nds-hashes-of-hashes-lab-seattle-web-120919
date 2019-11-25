2e16b84559d167ea2fd845a5489f9a02ca25bf89
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2019-11-25 17:18:28 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -9,7 +9,10 @@
    sub_category: {
      label: "Phylum",
      sub_category: {
-       label: "Class"
+       label: "Class",
+        sub_category: {
+          label: "Order"
+        }
      }
    }
  }
